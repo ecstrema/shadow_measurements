0225fc2c67964ce605c612ef67a6da76ac62f746
Remi Marche <remimarche@gmail.com>
2022-09-25 19:14:58 +0200

    fix: compression and sizes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
-import compress from "vite-plugin-compress";
+import compress from "vite-plugin-compression";
 
 export default defineConfig({
   base: "/shadow_measurements/",
   plugins: [
     compress(),
   ],
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ["three"],
+        },
+      },
+    },
+  },
 });
